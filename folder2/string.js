c0812725b67d4cba5eac4636b5bf1811a5b789f7
Nguyễn Văn Anh <anh9701zt@gmail.com>
2025-06-25 12:17:18 +0700

    đã chỉnh sửa xong

---
+++ folder2/string.js
@@ -0,0 +1,13 @@
+console.log('Xin chào mình là Nguyễn Văn Anh')
+
+let a = 'Nguyễn Hồng Nhung - 2008   '
+console.log(a)
+console.log(4+5+'a')
+console.log('a' + 6 + 7)
+console.log(`  Nguyễn Hồng Nhung - 2008  `)
+
+// lấy từ trong chuỗi
+let x = a.slice(0,6) // lấy đc từ 'Nguyễn'
+a.trim() // xóa khoảng trắng đầu và cuối chuỗi
+x = a.split('-')
+console.log(x)
